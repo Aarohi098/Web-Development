0baa31917172a87fb4a902facafecccc71a1146f
Aarohi098 <aarohimahajan007@gmail.com>
2025-06-09 18:46:36 +0100

    Lesson 19 complete

---
+++ Lesson 19/script.js
@@ -0,0 +1,25 @@
+document.body.style.backgroundColor = "lightblue";
+document.getElementById("myheading").innerHTML = "Heading from JavaScript";
+
+/*
+/*Variables
+*/
+//Keywords
+
+var num_1 = 30; // number
+var num_2 = 20; // number
+var name = "Aarohi"; // string
+
+document.getElementById("add_result").innerHTML = num_1 + num_2;
+document.getElementById("substract_result").innerHTML = num_1 - num_2;
+
+document.getElementById("square").innerHTML = 19 * 19;
+
+document.getElementById("square_root").innerHTML = 361 ** 0.5;
+
+document.getElementById("add_result").innerHTML = num_1 + num_2;
+
+// Assignment Operators
+var x = 100;
+x += 5; //x = x+5
+document.getElementById("result").innerHTML = x % 9;
