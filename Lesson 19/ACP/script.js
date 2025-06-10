e420033688e12ba97830b72c611c441b07c177aa
Aarohi098 <aarohimahajan007@gmail.com>
2025-06-10 19:09:56 +0100

    Lesson 18 ACP complete

---
+++ Lesson 19/ACP/script.js
@@ -0,0 +1,17 @@
+document.body.style.backgroundColor = "lightgreen";
+
+var add_1 = 314
+var add_2 = 209
+document.getElementById("add").innerHTML = add_1 + add_2;
+
+var add_3 = 23650
+var add_4 = 5300
+document.getElementById("add2").innerHTML = add_3 + add_4;
+
+var sub_1 = 23
+var sub_2 = 78
+document.getElementById("sub1").innerHTML = sub_1 - sub_2;
+
+var sub_3 = 664
+var sub_4 = 193
+document.getElementById("sub2").innerHTML = sub_3 - sub_4;
