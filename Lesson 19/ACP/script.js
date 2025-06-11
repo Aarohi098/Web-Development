7e5f2e1de7d1cffd4fe881952cc7a820b3b9bb4b
Aarohi098 <aarohimahajan007@gmail.com>
2025-06-11 16:23:50 +0100

    Lesson 19 ACP complete

--- Lesson 19/ACP/script.js
+++ Lesson 19/ACP/script.js
@@ -15,3 +15,31 @@ document.getElementById("sub1").innerHTML = sub_1 - sub_2;
 var sub_3 = 664
 var sub_4 = 193
 document.getElementById("sub2").innerHTML = sub_3 - sub_4;
+
+var times_1 = 47
+var times_2 = 29
+document.getElementById("times1").innerHTML = times_1 * times_2;
+
+var times_3 = 357
+var times_4 = 93
+document.getElementById("times2").innerHTML = times_3 * times_4;
+
+var div_1 = 538
+var div_2 = 16
+document.getElementById("div1").innerHTML = div_1 / div_2;
+
+var div_3 = 445
+var div_4 = 5
+document.getElementById("div2").innerHTML = div_3 / div_4;
+
+var sq_1 = 67
+document.getElementById("sq1").innerHTML = sq_1 * sq_1;
+
+var sq_2 = 192
+document.getElementById("sq2").innerHTML = sq_2 * sq_2 * sq_2;
+
+var ro_1 = 225
+document.getElementById("ro1").innerHTML = ro_1 ** 0.5
+
+var ro_2 = 678
+document.getElementById("ro2").innerHTML = ro_2 ** 0.5
