96f67035156b3f51b64a2e699b1087d468c9489b
Aarohi098 <aarohimahajan007@gmail.com>
2025-06-11 18:46:29 +0100

    Lesson 20 complete

---
+++ Lesson 20/script.js
@@ -0,0 +1,24 @@
+function add() {
+    var number1 = document.getElementById("num1").value;
+    var number2 = document.getElementById("num2").value;
+    var result = parseFloat(number1) + parseFloat(number2);
+    document.getElementById("result").innerHTML = result;
+}
+
+function substract() {
+    var number1 = document.getElementById("num1").value;
+    var number2 = document.getElementById("num2").value;
+    var result = parseFloat(number1) - parseFloat(number2);
+    document.getElementById("result").innerHTML = result;
+}
+
+function square(num) {
+    var b = 5;
+    return num * num + a + b;
+}
+
+var a = 3;
+document.writeln(square(a) + "<br>");
+document.writeln(square(a) + "<br>");
+document.writeln("Value of: "(b))
+
