63546cda01cfaa9aa8d8b4bd498c215abb7108bf
Aarohi098 <aarohimahajan007@gmail.com>
2025-07-16 19:22:52 +0100

    Lesson 26 ACP complete

--- Lesson 21/ACP/script.js
+++ Lesson 21/ACP/script.js
@@ -1,8 +1,15 @@
 var number = "";
 
-for (i = 1; i < 11; i++) {
+for (var i = -10; i < 11; i++) {
     number += i + "<br>"
 }
+document.writeln(number);
+
+var random_number = Math.random() * 500;
+var floor_number = Math.floor(random_number);
+document.writeln(random_number + " <br>");
+document.writeln(floor_number + " <br>");
+
 
 document.writeln("The random value is " + Math.floor(Math.random() * 100) + "<br>");
 
