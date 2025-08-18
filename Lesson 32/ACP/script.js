fd017435fcbbad06bbbce03568d7136ce81584e7
Aarohi098 <aarohimahajan007@gmail.com>
2025-08-18 18:58:00 +0100

    Lesson 32 ACP complete

---
+++ Lesson 32/ACP/script.js
@@ -0,0 +1,10 @@
+function randomise(event) {
+    event.preventDefault();
+
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    let randomColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.body.style.backgroundColor = randomColor;
+}
