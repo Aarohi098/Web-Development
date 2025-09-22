f1d0a53545e02a69150aa93a1340a99f3913cf11
Aarohi098 <aarohimahajan007@gmail.com>
2025-09-22 17:15:38 +0100

    Lesson 39 complete

---
+++ Python/main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, World!")
+
+# Printing a name
+print("My name is Aarohi")
+
+# Printing a hobby
+print("I like to draw")
+
+# Printing a simple calculation
+print("The sum of 11 + 21", 11 + 21)
